22a143f39234077a1c364c28480aa8ba68a540cc
rajatTemaniya <rajat.temaniya@gmail.com>
2025-01-26 16:26:27 +0530

    Changes for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: "/rajatTemaniya/", // YOUR REPO NAME HERE  
+  build: {
+    outDir: 'dist', // Output folder
+  },
 })
